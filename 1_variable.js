7b8c9572d1b8363c5d5a75acc14715236393c4e6
kavita <schoolprojec265@gmail.com>
2024-07-26 21:43:18 +0530

    variable practice

---
+++ 1_variable.js
@@ -0,0 +1,10 @@
+const accountId=12565;
+var emailId="kavita@gmail.com" //it is not prefer to use because issue of block and functional scope
+let userName="kavitamaurya"
+city="jaipur"
+// accountId=2; //that is not allowed
+console.log(accountId);
+emailId="mahima@gmail.com"
+usaerName="mahima"
+city="ludhiana"
+console.table([accountId, emailId,userName,city])
