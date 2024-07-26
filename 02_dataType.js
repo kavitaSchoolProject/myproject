86bde56d5dcd230f0ba02ec8db86c814711b212d
kavita <schoolprojec265@gmail.com>
2024-07-26 22:05:17 +0530

    data type

---
+++ 02_dataType.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all js code as newer version
+// alert(3+3) //we are using nodejs , not brower
+
+console.log(3+6)
+console.log("mahima");
+
+let Id=5
+let userName="kavita"
+let isLogged=true;
+let state;
+let city=null;
+// all type dataType in js
+//number => 2 to power 53
+//bigint
+//string=>""
+//boolean => true/false
+//null => standalone value
+//undefined => not value define
+//symbol => unique
+
+//object
+ console.log(typeof "kavita")
+ console.log(typeof "userName");
+ console.log(typeof ID)
+ console.log(typeof isLogged)
+ console.log(typeof state)
+ console.log(typeof 3.14)
+ console.log(typeof city);
+ console.log(typeof undefined);
+ console.log(typeof Symbol);
+ console.log(typeof null)
